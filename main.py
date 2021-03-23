67c3a09f52f7d14fac9a9f573ef4429b89397bf4
sudoshmudo <55032335+sudoshmudo@users.noreply.github.com>
2021-03-23 16:07:15 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import json
+import os
+import subprocess
+import sys
+from types import SimpleNamespace as Namespace
+
+from fastapi import APIRouter, FastAPI
+import requests
+      
+app = FastAPI()
+
+CARD = '''BEGIN:VCARD
+VERSION:3.0
+N:{}
+NOTES:{}
+TITLE:{}
+END:VCARD'''
+
+@app.get("/{host}")
+async def get(host):
+    response = requests.get('http://{}/openapi.json'.format(host)).text
+    parsed = json.loads(response, object_hook=lambda d: Namespace(**d))
+    
+    cards = []
+    
+    paths = vars(parsed.paths)
+    for path_key in paths:
+    	methods = vars(paths[path_key])
+    	for method_key in methods:
+    	    names = methods[method_key].summary.split()
+    	    cards.append(CARD.format(methods[method_key].summary, path_key, method_key))
+    	    
+    return ''.join(cards)
