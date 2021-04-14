2101aec0d83d5a478c69493c61d70130ef2c49ff
rossum <rossum@tuta.io>
2021-04-14 15:31:10 +0200

    added icon server

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import subprocess
 import sys
 from types import SimpleNamespace as Namespace
 
+from dotenv import load_dotenv
 from fastapi import APIRouter, FastAPI
 import requests
+
+load_dotenv()
+
+ICON_SERVER = os.environ['ICON_SERVER']
       
 app = FastAPI()
 
@@ -13,6 +18,7 @@ CARD = '''BEGIN:VCARD
 VERSION:3.0
 N:{}
 TITLE:{}
+PHOTO;ENCODING=b;TYPE=image/png:{}
 END:VCARD'''
 
 @app.get("/{host}")
@@ -21,11 +27,14 @@ async def get(host):
     parsed = json.loads(response, object_hook=lambda d: Namespace(**d))
     
     cards = []
-    
+    images = {}
+
+    for tag in parsed.tags:
+        images[tag.name] = requests.get('{}/icons/{}'.format(ICON_SERVER, tag.name)).text[1:-1]
+
     paths = vars(parsed.paths)
     for path_key in paths:
-    	methods = vars(paths[path_key])
-    	for method_key in methods:
-    	    cards.append(CARD.format(methods[method_key].summary, path_key))
-    	    
+        methods = vars(paths[path_key])
+        for method_key in methods:
+            cards.append(CARD.format(methods[method_key].summary, path_key, images[methods[method_key].tags[0]]))
     return ''.join(cards)
